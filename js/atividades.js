392872cde0876063dd82232dcfb828ddf5c7bfbb
Giulianno Sande Monteiro <91056214+Giusande@users.noreply.github.com>
2024-11-20 18:24:26 -0300

    Add files via upload

---
+++ js/atividades.js
@@ -0,0 +1,176 @@
+const atvd = [
+    {
+        title: "Guerra civil",
+        desc: "Trabalho feito em duas etapa, mapa mental e apresentação em slide. Nesse aqui, podemos ver o mapa mental referente ao trabalho.",
+        typeAtvd: "Mapa mental",
+        data: "**/**/****",
+        img: "https://drive.google.com/file/d/1Fu-cQHycwC8V5uVOmEsmv8DG3Xm3VVaC/preview",
+        link: "https://drive.google.com/file/d/1Fu-cQHycwC8V5uVOmEsmv8DG3Xm3VVaC/preview",
+        materia: "humanas",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Guerra civil",
+        desc: "Trabalho feito em duas etapa, mapa mental e apresentação em slide. Nesse aqui, podemos ver o slide referente ao trabalho.",
+        typeAtvd: "Slide",
+        data: "**/**/****",
+        img: "https://drive.google.com/file/d/1LVArWZ8k-aoBDp3fCrCkMMr2dv7sBq61/preview",
+        link: "https://drive.google.com/file/d/1LVArWZ8k-aoBDp3fCrCkMMr2dv7sBq61/preview",
+        materia: "humanas",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Resumo do livro",
+        desc: "Nessa atividade, foi pedido para que os alunos fizessem um resumo sobre uma determinada página do livro.",
+        typeAtvd: "Resumo",
+        data: "29-02-2024",
+        img: "https://drive.google.com/file/d/1K6o2PCslBRDXEAwqu9O6h6FxlhAgvXgU/preview",
+        link: "https://drive.google.com/file/d/1K6o2PCslBRDXEAwqu9O6h6FxlhAgvXgU/preview",
+        materia: "humanas",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Página do livro",
+        desc: "Atividade do livro de Ciencias Humanas da página 10 até 38. Não tiveram dó dos alunos, até porque quem tem dó é piano.",
+        typeAtvd: "Livro",
+        data: "**/**/****",
+        img: "https://drive.google.com/file/d/17LRpEtu6VABUmz7hZ5xxTDtSIZoRHDs4/preview",
+        link: "https://drive.google.com/file/d/17LRpEtu6VABUmz7hZ5xxTDtSIZoRHDs4/preview",
+        materia: "humanas",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Ficha de Natureza",
+        desc: "Ficha da matéria de Ciências da Natureza, com o assunto de Força Magnétca e Indução Elétromagnética. Realizado em sala.",
+        typeAtvd: "Ficha",
+        data: "**/**/****",
+        img: "https://drive.google.com/file/d/1AgnTnVn_WPpfXkdWJa3ZZAIimGB-0ai6/preview",
+        link: "https://drive.google.com/file/d/1AgnTnVn_WPpfXkdWJa3ZZAIimGB-0ai6/preview",
+        materia: "natureza",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Relatório de atividade",
+        desc: "Relatório de uma atividade sobre uma experiência realizada por toda sala no patio do colégio. ",
+        typeAtvd: "Relatório",
+        data: "**/**/****",
+        img: "https://docs.google.com/document/d/1vrMSwW_1ft9YbHeEiu8D5KCq9W_r1BiCp_0YZqFcn9Q/edit?usp=sharing",
+        link: "https://docs.google.com/document/d/1vrMSwW_1ft9YbHeEiu8D5KCq9W_r1BiCp_0YZqFcn9Q/edit?usp=sharing",
+        materia: "natureza",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Resumo do Livro",
+        desc: "Resumo do livro “Um hotel na esquina do tempo”. Um Hotel na Esquina do Tempo é um romance de Jamie Ford",
+        typeAtvd: "Resumo",
+        data: "**/**/****",
+        img: "https://docs.google.com/document/d/1Hp-D_e2er1smtUanMcrJdkCMBqY8OKVOx-kWjBlC7mA/edit",
+        link: "https://docs.google.com/document/d/1Hp-D_e2er1smtUanMcrJdkCMBqY8OKVOx-kWjBlC7mA/edit",
+        materia: "linguagens",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Torquato Neto",
+        desc: "Torquato Neto foi um poeta, jornalista e letrista brasileiro, nascido em Teresina, Piauí, em 9 de novembro de 1944",
+        typeAtvd: "Atividade",
+        data: "**/**/****",
+        img: "https://drive.google.com/file/d/17V7iwqh35R_NJ6n7uVPC8ltObnk3f15M/preview",
+        link: "https://drive.google.com/file/d/17V7iwqh35R_NJ6n7uVPC8ltObnk3f15M/preview",
+        materia: "linguagens",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Páginas do livro",
+        desc: "Páginas do livro de linguagens. Atividades realizadas tanto em classe, tanto em casa. COM VISTO!",
+        typeAtvd: "Livro",
+        data: "**/**/****",
+        img: "https://drive.google.com/file/d/1-OsF6kNoTo55YLEbjDvmVGCODZmVhMj_/preview",
+        link: "https://drive.google.com/file/d/1-OsF6kNoTo55YLEbjDvmVGCODZmVhMj_/preview",
+        materia: "linguagens",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Caderno",
+        desc: "Foto do caderno comprovando que prestei atençaõ na aula e anotei o que tinha para anotar. COM VISTO!",
+        typeAtvd: "",
+        data: "**/**/****",
+        img: "https://drive.google.com/file/d/1j0YaNeBBSLL_zewdkgW9AYJPIwqHCHOS/preview",
+        link: "https://drive.google.com/file/d/1j0YaNeBBSLL_zewdkgW9AYJPIwqHCHOS/preview",
+        materia: "linguagens",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Ficha de Linguagens",
+        desc: "Ficha de estudo de linguagens da professora Andresa Rodrigues. Atividade realizada em classe e COM VISTO!",
+        typeAtvd: "",
+        data: "**/**/****",
+        img: "https://drive.google.com/file/d/1YPzQSe1KvsiaybWtwaxq_uRUp1xQ_hKG/preview",
+        link: "https://drive.google.com/file/d/1YPzQSe1KvsiaybWtwaxq_uRUp1xQ_hKG/preview",
+        materia: "linguagens",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Atividade do Livro",
+        desc: "Atividade do Livro de matemátiica 'você é o autor' realizada em classe e COM VISTO!",
+        typeAtvd: "Livro",
+        data: "**/**/****",
+        img: "https://drive.google.com/file/d/1otateGmgwbFHzFbf4SiuMWQDPoxPLvQi/preview",
+        link: "https://drive.google.com/file/d/1otateGmgwbFHzFbf4SiuMWQDPoxPLvQi/preview",
+        materia: "matematica",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Ficha de matemática",
+        desc: "Ficha de matemática e suas tecnoligias realizada em classe e COM VISTO!",
+        typeAtvd: "Ficha",
+        data: "*/**/****",
+        img: "https://drive.google.com/file/d/1oSe9ofQU-3w94hhjT-zemyPPpN68iRgR/preview",
+        link: "https://drive.google.com/file/d/1oSe9ofQU-3w94hhjT-zemyPPpN68iRgR/preview",
+        materia: "matematica",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Atividade do Livro",
+        desc: "Atividade do Livro de matemátiica 'você é o autor' realizada em classe e COM VISTO!",
+        typeAtvd: "Livro",
+        data: "**/**/****",
+        img: "https://drive.google.com/file/d/1ot3idFNOLtDALiIWt5yjzOH3enpqBkl8/preview",
+        link: "https://drive.google.com/file/d/1ot3idFNOLtDALiIWt5yjzOH3enpqBkl8/preview",
+        materia: "matematica",
+        ano: "3",
+        unidade: "2"
+    },
+    {
+        title: "Ficha de matemática",
+        desc: "Ficha de matemática e suas tecnoligias realizada em classe e COM VISTO!",
+        typeAtvd: "Ficha",
+        data: "**/**/****",
+        img: "https://drive.google.com/file/d/1opzVyAXyPc2nzsiWgb1gglS9t_QVJEJA/preview",
+        link: "https://drive.google.com/file/d/1opzVyAXyPc2nzsiWgb1gglS9t_QVJEJA/preview",
+        materia: "matematica",
+        ano: "3",
+        unidade: "2"
+    },
+    {title: "Trabalho de Mecânica Quântica(digital)",desc: "Um Mergulho no Mundo Subatômico Um trabalho de mecânica quântica.",typeAtvd: "Ficha",data: "2024-11-14",img: "https://docs.google.com/document/d/1U-Lt0bb9EHEsVSfer40omgvz6NzKo9aMjxYAfmEeBN4/preview",link: "https://docs.google.com/document/d/1U-Lt0bb9EHEsVSfer40omgvz6NzKo9aMjxYAfmEeBN4/preview",materia: "natureza",ano: "3",unidade: "4"},
+    {title: "Apresentação Mecânica Quântica",desc: "A Mecânica Quântica é a teoria física que descreve o comportamento da matéria em escala atômica e subatômica.",typeAtvd: "slide",data: "2024-11-07",img: "https://docs.google.com/presentation/d/1I9qFKiUpXxteHC6W49ckRFyxl4_ZM7CLULjjFKu8XSY/preview",link: "https://docs.google.com/presentation/d/1I9qFKiUpXxteHC6W49ckRFyxl4_ZM7CLULjjFKu8XSY/preview",materia: "natureza",ano: "3",unidade: "4"},
+    {title: "Relatório do Museu de ciências nucleares (UFPE)",desc: "O Museu de Ciências Nucleares da UFPE, em Recife, é um espaço dedicado à divulgação científica da energia nuclear. ",typeAtvd: "Ficha",data: "2024-10-17",img: "https://docs.google.com/document/d/1_61rn_rhTTTnpUYdFxdd_A-zd9yNbXYBDTy_pvKqjKE/preview",link: "https://docs.google.com/document/d/1_61rn_rhTTTnpUYdFxdd_A-zd9yNbXYBDTy_pvKqjKE/preview",materia: "natureza",ano: "3",unidade: "4"},
+    {title: "Ficha de maura",desc: "Ficha feita em dupla com vinicius pois, tinha chegado atrasado. ",typeAtvd: "Ficha",data: "2024-11-08",img: "https://drive.google.com/file/d/1iuOMgzWFNRTEq6ky-gDOJODQnsSEgeRJ/preview",link: "https://drive.google.com/file/d/1iuOMgzWFNRTEq6ky-gDOJODQnsSEgeRJ/preview",materia: "natureza",ano: "3",unidade: "4"},
+    {title: "Trabalho de Mecânica Quântica(pautado)",desc: "Um Mergulho no Mundo Subatômico Um trabalho de mecânica quântica.",typeAtvd: "Ficha",data: "2024-11-14",img: "https://drive.google.com/file/d/1O5oNxbHoat1o9NeS8L-VQGnzgHi0c-Z1/preview",link: "https://drive.google.com/file/d/1O5oNxbHoat1o9NeS8L-VQGnzgHi0c-Z1/preview",materia: "natureza",ano: "3",unidade: "4"},
+    {title: "Relatório dos seminarios",desc: "Relatório dos seminários apresentados no início da 4° unidade curricular da matéria de ciência da natureza. ",typeAtvd: "Ficha",data: "2024-11-14",img: "https://docs.google.com/document/d/1TElfoS5DrXBaFWPOla9hZAiHHLr8v_JatM4P7Rr_4Vg/preview",link: "https://docs.google.com/document/d/1TElfoS5DrXBaFWPOla9hZAiHHLr8v_JatM4P7Rr_4Vg/preview",materia: "natureza",ano: "3",unidade: "4"},
+    {title: "Relatório do filme Interestelar",desc: "Relatório do filme Interestelar relacionado com o capítulo 1 do livro de Ciências da natureza. ",typeAtvd: "Ficha",data: "2024-11-14",img: "https://docs.google.com/document/d/1--9WCOU-QCMN3tWV8OeM5QsI49kbH95MBwkQZl6N-Hk/preview",link: "https://docs.google.com/document/d/1--9WCOU-QCMN3tWV8OeM5QsI49kbH95MBwkQZl6N-Hk/preview",materia: "natureza",ano: "3",unidade: "4"}
+]
+
+export default atvd;
